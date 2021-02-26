6ec3effe486dc05ca3e32bc988cdf1d3d99f1f7d
Eli0ttD0natien <eliott.donatien@viacesi.fr>
2021-02-26 09:55:48 +0100

    Added Authentification

--- test/test.js
+++ test/test.js
@@ -4,6 +4,20 @@ const index = require("../src/index.js");
 const expect = require("chai").expect;
 const baseUrl = 'http://vm-dev-central4';
 
+async function authenticate(){
+    let identifiants = "edonatien:S34l4dm";
+    let buff = Buffer.from(identifiant, 'utf-8');
+    let headers = {
+        Authorization: "Basic" + buff.toString('base64')
+    }
+    let resp = await axios.put(baseUrl + '/authenticate', headers).then((response) => {
+        return response;
+    }).catch((err) => {
+        console.log(err);
+    })
+    console.log(resp);
+}
+
 async function getRequest(endpoint){
     let resp = await axios.get(baseUrl + endpoint).then((response) => {
         return response;
